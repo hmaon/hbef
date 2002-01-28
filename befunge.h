3bafab810d8a36b6676570f95bd198206bc52c60
Greg Velichansky <hmaon@bumba.net>
2002-01-28 15:57:04 +0000

    m93

--- befunge.h
+++ befunge.h
@@ -1,6 +1,9 @@
 #ifndef ___BEFUNGE_H
 #define ___BEFUNGE_H
 
+#include<stdio.h>
+#include<stdlib.h>
+
 typedef struct {
 	int x, y;
 } coord;
@@ -28,6 +31,7 @@ typedef struct{
 /* bits for the mode bitfield */
 #define mSTRING 0x1	           
 #define mTERM 0x2
+#define m93 0x4
 
 void strepl (stackt *, int);
 void stpush (stackt *, int);
@@ -43,13 +47,16 @@ int pop (bef_interp *);
 bef_interp *bef_allocinterp (int stack, int w, int h);
 void bef_free (bef_interp *);
 
+void bef_reset (bef_interp *);
+
 void bef_do (bef_interp *, char);
 int bef_step (bef_interp *);
+int bef_run (bef_interp *);
 
 int strtoicpy (int *, char *);
 
 int bef_loadcode (bef_interp *, FILE *);
 int bef_load93 (bef_interp *, char *name);
-bef_interp *bef_loadhb (char *name);
+bef_interp *bef_loadh (char *name);
 
 #endif
